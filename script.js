918b73d21e35e264eea5bb055575a3bd42f3480a
AndrewCooper <Cooper.aje@gmail.com>
2022-01-16 23:46:29 +0000

    Get weather for week and display to user.

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-import getCurrentWeatherAsync from "./Services/WeatherServices";
-import getCurrentWeatherComponent from "./Components/WeatherComponents";
+import { getCurrentWeatherAsync, getWeatherForWeekAsync } from "./Services/WeatherServices";
+import { getCurrentWeatherComponent, getFutureWeatherComponent } from "./Components/WeatherComponents";
 
 const form = document.querySelector('#search-city');
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
+
+    let weatherRow = document.querySelector(".weather-row");
+    weatherRow.innerHTML = '';
     
     const formData = new FormData(event.target);
     const city = formData.get('city-input');
 
     const currentWeather = await getCurrentWeatherAsync(city);
     getCurrentWeatherComponent(currentWeather);
-})
+
+    const {lon, lat} = getLonLatFromWeather(currentWeather);
+
+    const futureWeather = await getWeatherForWeekAsync(lon, lat);
+    const {daily} = futureWeather;
+
+    daily.forEach((day, i) => {
+        if(i == 0) {
+            return;
+        }
+
+        getFutureWeatherComponent(day);
+    });
+});
+
+function getLonLatFromWeather(weather){
+    const {coord:{lat}, coord:{lon}} = weather;
+
+    return {lon, lat};
+}
